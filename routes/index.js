ae291e7e8b4f88a6ce3b7eec556a5d9fa5139dfb
Antoine Foggs <fireheartfoggs@gmail.com>
2018-07-01 19:40:35 -0500

    Got code to work to properly change cities when selected.

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,86 @@
 let express = require('express');
 let router = express.Router();
+let calls = 0;
 // variable for rpn
 const rpn = require('request-promise-native');
 const key = process.env.STOCK_KEY;
+const bodyParser = require("body-parser");
+let city = "Chicago";
+
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on req.body
+ */
+bodyParser.urlencoded({
+    extended: true
+});
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+bodyParser.json();
+
+
+
 
 // create function to call api
-const options = {
-  url:`http://api.wunderground.com/api/${ key }/conditions/q/IL/Chicago.json`,
+let options = {
+  url:`http://api.wunderground.com/api/${ key }/conditions/q/IL/` + city + `.json`,
   headers:  {
     'User-Agent': 'Request-Promise'
   },
   json: true
 };
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   rpn(options).then( function(results) {
     // render profile
-    // Grabbing the City, Weather, Last Update, Temperature in F, Real Feel in F, Rain in Inches, Humidity, and Heat index in F.
-    res.render('index', { title: `Weather Conditions in ${ results.current_observation.display_location.full }`, weather: results.current_observation.weather,
-      update: results.current_observation.observation_time, temp: results.current_observation.temp_f, realfeel: results.current_observation.feelslike_f,
-      humidity: results.current_observation.relative_humidity, rain: results.current_observation.precip_today_in, heat: results.current_observation.heat_index_f,
-      tempc: results.current_observation.temp_c, realfeelc: results.current_observation.feelslike_c, heatc: results.current_observation.heat_index_c});
+    const observations = results.current_observation; //Prepend for Data gathering
+    // Grabbing the City, Weather, Last Update, Temperature in F, Real Feel in F, Rain in Inches, Humidity, and Heat index in F for Chicago
+    res.render('index', { title: observations.display_location.full, weather: observations.weather, update: observations.observation_time,
+    temp: observations.temp_f, realfeel: observations.feelslike_f, humidity: observations.relative_humidity, rain: observations.precip_today_in,
+    heat: observations.heat_index_f, tempc: observations.temp_c, realfeelc: observations.feelslike_c, heatc: observations.heat_index_c});
   });
 });
 
+/* POST web page */
+router.post("/", function (req, res, next) {
+  let citySelect = req.body.city;
+  console.log(citySelect);
+
+  // create function to call api
+  let options = {
+    url:`http://api.wunderground.com/api/${ key }/conditions/q/IL/` + citySelect + `.json`,
+    headers:  {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true
+  };
+  
+  rpn(options).then( function(results) {
+    // render profile
+    const observations = results.current_observation; //Prepend for Data gathering
+    // Grabbing the City, Weather, Last Update, Temperature in F, Real Feel in F, Rain in Inches, Humidity, and Heat index in F for Chicago
+    res.render('index', { title: observations.display_location.full, weather: observations.weather, update: observations.observation_time,
+    temp: observations.temp_f, realfeel: observations.feelslike_f, humidity: observations.relative_humidity, rain: observations.precip_today_in,
+    heat: observations.heat_index_f, tempc: observations.temp_c, realfeelc: observations.feelslike_c, heatc: observations.heat_index_c});
+  });
+
+
+});
+
+
+console.log("hello");
 
 module.exports = router;
+
+//Automate calls with a limit of 4 calls, one every 15 minutes
+/*function intervalFunc() {
+    console.log();
+     }
+    setInterval(intervalFunc,5000);*/
+
 
 
 // console.log('Request URL value:', request_url);
@@ -73,3 +127,17 @@ module.exports = router;
 // block.appendChild(temp);
 // //
 // });
+
+/*
+router.post("/", function (req, res, next) {
+    rpn(options).then( function(results) {
+      // render profile
+      const observations = results.current_observation; //Prepend for Data gathering
+      // Grabbing the City, Weather, Last Update, Temperature in F, Real Feel in F, Rain in Inches, Humidity, and Heat index in F for Chicago
+      res.render('index', { title: observations.display_location.full, weather: observations.weather, update: observations.observation_time,
+      temp: observations.temp_f, realfeel: observations.feelslike_f, humidity: observations.relative_humidity, rain: observations.precip_today_in,
+      heat: observations.heat_index_f, tempc: observations.temp_c, realfeelc: observations.feelslike_c, heatc: observations.heat_index_c});
+    });
+
+});
+*/
